c5c775e736ae0d0aaf1b9266f4117d6569977a21
Aurelio John Bautista <nox.elle.412@gmail.com>
2026-06-03 15:35:59 +0800

    additional fields

--- app/Models/Travel.php
+++ app/Models/Travel.php
@@ -4,10 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Travel extends Model
 {
-    use HasFactory;
+    use HasFactory, softDeletes;
+
+     protected $table = 'travels';
 
     protected $fillable = [
         'share_token',
