ebb23b14faaab34c248717fb4257bb43e3e9ce73
Aurelio John Bautista <nox.elle.412@gmail.com>
2026-06-04 08:32:57 +0800

    all tab

--- resources/views/travels/all.blade.php
+++ resources/views/travels/all.blade.php
@@ -48,6 +48,7 @@
                     <th>#</th>
                     <th>Mode</th>
                     <th>Route</th>
+                    <th>Transport Provider</th>
                     <th>Purpose / Project</th>
                     <th>Date</th>
                     <th>Pax</th>
@@ -69,6 +70,9 @@
                         <strong>{{ $travel->origin }}</strong>
                         <span style="color:#a0aec0;padding:0 .4rem">→</span>
                         <strong>{{ $travel->destination }}</strong>
+                    </td>
+                    <td>
+                        <strong>{{ $travel->transport_provider }}</strong>
                     </td>
                     <td>{{ $travel->purpose }}</td>
                     <td style="white-space:nowrap">
